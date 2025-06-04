5b69605a9926d5476b72b2a44ffb535964717f63
Sebastian Lenz <github@sebastian-lenz.de>
2025-06-04 14:42:56 +0200

    Initialize the foreign query listener with the plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,7 @@ namespace lenz\openinghoursfield;
 
 use craft\events\RegisterComponentTypesEvent;
 use craft\services\Fields;
+use lenz\craft\utils\foreignField\ForeignFieldQueryListener;
 use lenz\openinghoursfield\fields\OpeningHoursField;
 use yii\base\Event;
 
@@ -21,6 +22,8 @@ class Plugin extends \craft\base\Plugin
    */
   public function __construct($id, $parent = null, array $config = []) {
     parent::__construct($id, $parent, $config);
+
+    ForeignFieldQueryListener::init();
 
     Event::on(Fields::class, Fields::EVENT_REGISTER_FIELD_TYPES, function(RegisterComponentTypesEvent $event) {
       $event->types[] = OpeningHoursField::class;
